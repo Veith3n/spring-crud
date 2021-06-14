fbeba2702f1edb0ca08fcfa66b49fd8f79a8de84
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2021-06-14 20:29:19 +0200

    Add happy path tests for cars endpoints

--- src/test/java/com/wcy/tai/lab1/Lab1ApplicationTests.java
+++ src/test/java/com/wcy/tai/lab1/Lab1ApplicationTests.java
@@ -1,13 +1,60 @@
 package com.wcy.tai.lab1;
 
+import com.wcy.tai.lab1.dtos.CarRequest;
+import com.wcy.tai.lab1.dtos.CarResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Lab1ApplicationTests {
+  @LocalServerPort
+  private int port;
 
-    @Test
-    void contextLoads() {
-    }
+  @Autowired
+  private TestRestTemplate restTemplate;
 
+  @Test
+  public void addsAndListCars() throws Exception {
+    var baseUrl = "http://localhost:" + port;
+    var carDto = new CarRequest(1, "foo");
+
+    // DB has empty state
+    var cars = getCars(baseUrl);
+    assertThat(cars.length).isEqualTo(0);
+
+    // Creates car
+    createCar(baseUrl, carDto);
+
+    // Returns car
+    cars = getCars(baseUrl);
+
+    assertThat(cars.length).isEqualTo(1);
+    assertThat(cars[0].getModel()).isEqualTo("foo");
+  }
+
+  private CarResponse[] getCars(String baseUrl) {
+    var carGetUrl = baseUrl + "/";
+
+    var responseEntity = restTemplate.getForEntity(carGetUrl, CarResponse[].class);
+
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    return responseEntity.getBody();
+  }
+
+  private void createCar(String baseUrl, CarRequest carDto) {
+    var carPostUrl = baseUrl + "/add";
+    var body = new HttpEntity<>(carDto);
+
+    var res = restTemplate.postForEntity(carPostUrl, body, void.class);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
 }
